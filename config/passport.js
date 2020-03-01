ae3e8a2176fa045ba7de15f3f68867b3d71627d9
couldbefree <zoriaalexandr@gmail.com>
2020-03-01 10:22:33 +0200

    made login

--- config/passport.js
+++ config/passport.js
@@ -14,8 +14,29 @@ module.exports = function(passport) {
           if(!user) {
             return done(null, false, { message: 'That email is not registered' })
           }
+
+          // Match password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+
+            if(isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Password incorrect' })
+            }
+          })
         })
         .catch(err => console.log(err))
     })
-  )
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 };
